8ccc7e096062884b9db91ddf9f8bc2c80c5d9e01
PinkiKumari16 <pinkikmr2672002@gmail.com>
2024-05-22 09:47:08 +0530

    deleted files

--- Devouring_Donuts.js
+++
@@ -1,3 +0,0 @@
-const input = require('readline-sync');
-let [x,y] = input.question('x,y: ').split(' ');
-console.log(x*y)
--- morning-run.js
+++
@@ -1,7 +0,0 @@
-let input = require('readline-sync');
-let [h,w] = input.question('h,w: ').split(' ').map(Number);
-if(2*(h+w)>=1000){
-    console.log('Yes');
-}else{
-    console.log('No');
-}
