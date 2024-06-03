1955dc6dd930656157d62ec726fd1f00479c35ef
pinkikumari16 <pinkikmr2672002@gmail.com>
2024-06-03 17:24:10 +0530

    fetch data from json files

--- src/components/our viewers/Main.jsx
+++ src/components/our viewers/Main.jsx
@@ -1,33 +1,14 @@
 import React, { useState } from 'react'
 import OureViewers from './OureViewers'
-// import { data } from './data.json'
+import dataJson  from './data.json'
 
 
 export const Main = () => {
     let [index, setIndex] = useState(0);
-    const data = [
-        {
-            "image" : "./pinki.jpeg" ,
-            "name" : "Pinki",
-            "professtion" : "FRONT END DEVELOPER",
-            "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
-        },
-        {
-            "image" : "./rinki.jpeg" ,
-            "name" : "Rinki",
-            "professtion" : "FULL STACK DEVELOPER",
-            "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
-        },
-        {
-            "image" : "./family.jpeg" ,
-            "name" : "Kiran",
-            "professtion" : "MEARN DEVELOPER",
-            "content" : "I'am baby meggings health goth +1, Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humlebrag pickled coloring book salvia hoodle, cold-pressed four doliar toast everyday carry."
-        }
-    ] 
+    console.log(dataJson)
   return (
     <>
-        <OureViewers name = {data[index].name} index={index} setIndex={setIndex} professtion={data[index].professtion} content={data[index].content} image={data[index].image} />
+        <OureViewers name = {dataJson[index].name} index={index} setIndex={setIndex} professtion={dataJson[index].professtion} content={dataJson[index].content} image={dataJson[index].image} />
     </>
   )
 }
